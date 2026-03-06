653a42ca4b4738257dd3e5f25e662efd0b3ac7f7
aligatorr197 <as2258597@gmail.com>
2026-03-06 18:56:42 +0530

    Initialize particles.js for background effects

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-function showMessage() {
-    alert("Thank you for visiting my portfolio!");
-}
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 80
+    },
+    "color": {
+      "value": "#38bdf8"
+    },
+    "shape": {
+      "type": "circle"
+    },
+    "opacity": {
+      "value": 0.5
+    },
+    "size": {
+      "value": 3
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#38bdf8",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 3
+    }
+  }
+});
